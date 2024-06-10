616264cfd974fb9ce76b36249b2218f88b7cda5b
Максим Ландау <12landay@mail.ru>
2024-06-10 23:20:59 +0300

    Readywe

--- bankruptcy_crm/settings.py
+++ bankruptcy_crm/settings.py
@@ -29,7 +29,8 @@ ALLOWED_HOSTS = [
     '0.0.0.0',
     '127.0.0.1',
     '45.89.190.67',
-    '*'
+    '*',
+    'localhost'
 ] + os.getenv('DJANGO_ALLOWED_HOSTS', '').split(',')
 INTERNAL_IPS = [
     "127.0.0.1",
