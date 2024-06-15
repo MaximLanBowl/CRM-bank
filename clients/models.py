7cbfe759aa4863a0270695b0515c4da16a7767f3
Maxim <162016490+MaximLanBowl@users.noreply.github.com>
2024-06-15 10:23:53 +0300

    Update models.py

--- clients/models.py
+++ clients/models.py
@@ -5,6 +5,7 @@ class Client(models.Model):
     name = models.CharField(max_length=100, verbose_name='Имя')
     email = models.CharField(verbose_name='Почтовый адрес')
     phone = models.CharField(max_length=50, verbose_name='Номер телефона')
+    meeting = models.CharField(blank=True, null=True, verbose_name='Встреча/заседание')
     notes = models.TextField(blank=True, null=True, verbose_name='Дополнительная информация')
     pdf_file = models.FileField(upload_to='client_files/pdf', null=True, blank=True, verbose_name='PDF файл')
     xml_file = models.FileField(upload_to='client_files/xml', null=True, blank=True, verbose_name='XML файл')
